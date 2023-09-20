02392070411719e1f06e7ed4c8d7700b3a87e194
przqstn <alex091301@gmail.com>
2023-09-20 08:59:40 +0800

    requiring online sync of date

--- getCurrentDate.js
+++ getCurrentDate.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
         dataType: 'json',
         success: function(data) {
             const onlineDate = new Date(data.utc_datetime);
-            document.querySelector('.formDate').value = getFormattedDate(onlineDate);
+            const localDate = new Date();
+
+            if (areDatesDifferent(onlineDate, localDate)) {
+                displayDateMismatchError();
+            } else {
+                document.querySelector('.formDate').value = getFormattedDate(onlineDate);
+            }
         },
         error: function(error) {
             console.error('Error fetching online date:', error);
@@ -16,5 +22,19 @@ $(document).ready(function() {
         const month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
         const year = date.getFullYear();
         return `${month}/${day}/${year}`;
+    }
+
+    function areDatesDifferent(date1, date2) {
+        // Check if the difference in milliseconds is greater than a tolerance (e.g., 1000 milliseconds)
+        const tolerance = 1000; // Adjust as needed
+        return Math.abs(date1.getTime() - date2.getTime()) > tolerance;
+    }
+
+    function displayDateMismatchError() {
+        // Display an error message to the user
+        alert("Your device's date is not the same as the World Time API. You will be logged out.");
+        
+        // Redirect the user to logout.php
+        window.location.href = 'logout.php';
     }
 });
