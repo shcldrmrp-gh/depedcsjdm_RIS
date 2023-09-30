65ca0e195331a1730f8fb7289439be38814e816c
Austin <alex091301@gmail.com>
2023-09-30 20:41:58 +0800

    added sweetalert for loggin in

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>DepEd CSJDM Requisition and Issue Slip Form</title>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+    <link rel="icon" type="image/x-icon" href="logos/depedcsjdmlogo.png">
+    <link rel="stylesheet" href="homepage_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+</head>
+<body>
+</body>
+</html>
+
 <?php
     session_start();
 
@@ -28,25 +49,71 @@
             $_SESSION['userOffice'] = $row['userOffice'];
             $_SESSION['depedEmail'] = $row['depedEmail'];
             
-            // check account type so user will access webpage based on their account type
             $accountType = $row['accountType'];
             if ($accountType == 'End User') {
-                header("Location: endUser_webpage.php"); //page for enduser
+                echo'<script>
+                        Swal.fire({
+                            icon: "success",
+                            title: "LOGIN SUCCESSFUL!",
+                            text: "Redirecting to E-RIS Form...",
+                            showConfirmButton: false,
+                            timer: 3000, // 5 seconds
+                            timerProgressBar: true
+                        }).then(function() {
+                            window.location.href = "endUser_webpage.php"; // Replace with the desired destination page
+                        });
+                    </script>';
             } elseif ($accountType == 'User Manager') {
-                header("Location: usermanagement.php"); //change location page, sample.php only for testing
+                echo'<script>
+                        Swal.fire({
+                            icon: "success",
+                            title: "LOGIN SUCCESSFUL!",
+                            text: "Redirecting to user manager admin page...",
+                            showConfirmButton: false,
+                            timer: 3000, // 5 seconds
+                            timerProgressBar: true
+                        }).then(function() {
+                            window.location.href = "usermanagement.php"; // Replace with the desired destination page
+                        });
+                    </script>';
             } elseif ($accountType == 'Super Admin') {
-                header("Location: Accounts_inventory.php");// Handle superAdmin redirection here, change location page
+                echo'<script>
+                        Swal.fire({
+                            icon: "success",
+                            title: "LOGIN SUCCESSFUL!",
+                            text: "Redirecting to super admin page...",
+                            showConfirmButton: false,
+                            timer: 3000, // 5 seconds
+                            timerProgressBar: true
+                        }).then(function() {
+                            window.location.href = "Accounts_inventory.php"; // Replace with the desired destination page
+                        });
+                    </script>';
             } else {
-                echo '<script>
-                alert("Login failed. Invalid DepEd E-mail or password.")
-                window.location.href = "homepage.php";
+                echo'<script>
+                    Swal.fire({
+                        icon: "error",
+                        title: "LOGIN FAILED!",
+                        text: "Invalid DepEd E-mail or password.",
+                        confirmButtonColor: "#3085d6",
+                        confirmButtonText: "OK"
+                    }).then(function() {
+                        window.location.href = "homepage.php";
+                    });
                 </script>';
             }
         } else {
-            echo '<script>
-                alert("Login failed. Invalid DepEd E-mail or password.")
-                window.location.href = "homepage.php";
-                </script>';
+            echo'<script>
+                Swal.fire({
+                    icon: "error",
+                    title: "LOGIN FAILED!",
+                    text: "Invalid DepEd E-mail or password.",
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: "OK"
+                }).then(function() {
+                    window.location.href = "homepage.php";
+                });
+            </script>';
         }
     }
 ?>
