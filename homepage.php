ac48cc944ae57a705cc9b72bbb05af2d6465d77c
Austin <alex091301@gmail.com>
2023-09-30 20:41:41 +0800

    fixed spacing

--- homepage.php
+++ homepage.php
@@ -7,9 +7,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>DepEd CSJDM Requisition and Issue Slip Form</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <link rel="icon" type="image/x-icon" href="logos/depedcsjdmlogo.png">
     <link rel="stylesheet" href="homepage_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
