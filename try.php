ab9fa4d2d1d772b6b53043753786d99ac8316b00
tuturnok69 <117349567+tuturnok69@users.noreply.github.com>
2023-10-06 10:52:44 +0800

    asd

--- try.php
+++ try.php
@@ -80,3 +80,38 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
         
+
+let form = document.querySelector("#formContainer");
+        let btn = document.querySelector("#generatePDF");
+        
+        
+        var opt = {
+            margin:         [-30, -40, 0, -100],
+            filename:       'ris-form.pdf',
+            image:          { type: 'jpeg', quality: .95},
+            html2canvas:  { scale: 2, allowMagnification: false, width: 1850, height: 1720},
+            jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        };
+
+        btn.addEventListener('click', () => {
+            
+            let clonedForm = $(form).clone();
+            clonedForm.find('#btnRelease').hide();
+
+            $(form).find('select').each(function(index, originalSelect) {
+                let clonedSelect = $(clonedForm).find('select').eq(index);
+                
+                let selectedValue = $(originalSelect).val();
+                
+                $(clonedSelect).val(selectedValue);
+            });
+
+            $(form).append(clonedForm);
+
+            html2pdf().set(opt).from(form).save().then(() => {
+                
+                document.getElementById("btnRelease").click();
+            });
+
+            document.getElementById("btnRelease").style.display = "none";
+        });
