787f5e042063db9499e4871cb9ec536277326d1c
Austin <alex091301@gmail.com>
2023-10-12 13:01:36 +0800

    changed back button script

--- admin_risform.php
+++ admin_risform.php
@@ -32,7 +32,7 @@
         <nav>
             <ul>
                 <li><a href="https://drive.google.com/file/d/1FWGsxirhae-hqoREOf2w79bnjvN4JlAu/view?usp=sharing">CNAS</a></li>
-                <li><a href="javascript:history.back()">BACK</a></li>
+                <li><a href="javascript:void(0);" onclick="window.history.back();">BACK</a></li>
             </ul>
             <br>
         </nav>
